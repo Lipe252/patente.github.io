4360ec87b7e43ab729dac971f1bcb0446dc3bb94
vini <v9.fantinel@gmail.com>
2026-03-28 20:11:03 -0300

    atualizaçao

--- servidor.js
+++ servidor.js
@@ -1,1 +1,35 @@
+const express = require('express');
+const mysql = require('mysql2');
 
+const app = express();
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'patente'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Conectado ao MySQL');
+});
+
+app.get('/', (req, res) => {
+    res.send('API rodando');
+});
+
+app.get('/pessoas', (req, res) => {
+    db.query('SELECT * FROM PESSOA', (err, results) => {
+        if (err) return res.status(500).json(err);
+        res.json(results);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
